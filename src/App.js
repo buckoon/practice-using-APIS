e86de85f0b05c59b066c4cb4fb068b398a0e0bc7
Jeremy new computer <jeremy8@vt.edu>
2023-04-08 10:16:06 -0400

    Another practice API application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+ 
+
+
+
+  
+
+    const APP_ID ="a8eae6ae"
+    const APP_KEY="5b568d90ce92b186c7bb5e241f468679"
+    
+   
+   
+
+     useEffect(() =>{
+      getRecipes();
+     },[]);
+  
+     const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
+      const data = await response.json();
+      console.log(data);
+     }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" >
+        <input className="search-bar" type="text" />
+        <button  className="search-button" type="submit">Search</button>
+      
+      </form>
+     
+    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
