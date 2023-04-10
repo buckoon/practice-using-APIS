7ef189b713ee514f573af75a4a6791973707cf5e
Jeremy new computer <jeremy8@vt.edu>
2023-04-10 13:03:33 -0400

    Just finished the search functionality for querying

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,31 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 import {FaSearch} from 'react-icons/fa';
+import {useNavigate} from 'react-router-dom';
 
 
 function Search() {
+
+    const [input, setInput] = useState("");
+    const navigate = useNavigate();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate('/searched/'+input)
+
+    };
+
   return (
-    <FormStyle>
-        <input type="text"/>
+    <FormStyle onSubmit={submitHandler}>
+        <div>
+            <FaSearch></FaSearch>
+            <input 
+                onChange={(e) => setInput(e.target.value)} 
+                type="text" 
+                value={input}
+            />
+        </div>
+        
         
     </FormStyle>
   )
@@ -14,8 +33,12 @@ function Search() {
 
 const FormStyle = styled.form`
     margin: 0rem 20rem;
-    position: relative;
-    width: 100%;
+    
+    
+    div{
+        width: 100%
+        position: relative;
+    }
     input{
         border: none;
         background: linear-gradient(35deg, #494949, #313131);
@@ -25,6 +48,7 @@ const FormStyle = styled.form`
         border: none;
         border-radius: 1rem;
         outline: none;
+        width: 100%;
     }
     svg{
         position:absolute;
